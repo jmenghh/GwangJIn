061a6be77e890e133c7a9113a15362409dbb22e5
[jmenghh] <[hojhong8@gmail.com]>
2025-04-29 01:14:55 +0900

    지역화폐 O,X

--- 지역화폐확인.py
+++ 지역화폐확인.py
@@ -19,11 +19,16 @@ options.add_argument('--headless')
 options.add_argument('--disable-gpu')
 
 # 2. 크롬 드라이버 열기
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=options)
 driver.get('https://www.zeropay.or.kr/UI_HP_009_03.act')
 time.sleep(2)
 
-
+# 광진구 찾기
+gwangjin = WebDriverWait(driver, 20).until(
+    EC.visibility_of_element_located((By.XPATH, "//button[text()='광진구']"))
+)
+driver.execute_script('arguments[0].scrollIntoView(true);',gwangjin)
+gwangjin.click()
 # 3. 가게 이름 하나씩 검색
 for idx, row in df.iterrows():
     store_name = row['가게명']  # CSV에 가게명 열이 있다고 가정
@@ -37,6 +42,9 @@ for idx, row in df.iterrows():
     
     # (2) 검색 버튼 클릭
     search_button = driver.find_element(By.ID, 'btnSearch')
+    WebDriverWait(driver, 10).until(
+        EC.invisibility_of_element((By.ID, 'comAlertPopup'))
+    )
     search_button.click()
     WebDriverWait(driver, 20).until(
     EC.invisibility_of_element_located((By.CSS_SELECTOR, "aside.loading"))
@@ -45,28 +53,22 @@ for idx, row in df.iterrows():
 # (3) 검색 결과 확인
     tbody = driver.find_element(By.ID, 'tbTbody')
     rows = tbody.find_elements(By.TAG_NAME, 'tr')
-    found = False
     for row in rows:
-        cells = row.find_elements(By.TAG_NAME, 'td')  # find_elements로 여러 개의 td 요소를 가져옵니다.
-        if len(cells) > 0:  # 셀이 존재하는지 확인
-            print('결과존재')
-            if len(cells) > 2 and '광진구' in cells[2].text:  # cells[2]에서 텍스트를 가져옵니다.
-                df.at[idx, '지역화폐'] = '가능'
-                print('광진구존재')
-                break
-            else:
-                df.at[idx,'지역화폐'] = '불가'
-                print('광진구없음')
-                break
-        else:
-            df.at[idx, '지역화폐'] = '불가'
-            print('결과없음')
-            break
+        cells = row.find_elements(By.TAG_NAME, 'td')
+    if len(cells) > 2:  # 셀이 존재하는지 확인
+        print('결과존재')
+        df.at[idx, '지역화폐'] = 'O'
+    else:
+        print('결과없음')
+        df.at[idx, '지역화폐'] = 'X'
+           
 
             
 
 # 4. 결과 저장
-df = df[['가게명','지역화폐']]
+cols = list(df.columns)
+cols.insert(cols.index('가게명')+1,cols.pop(cols.index('지역화폐')))
+df = df[cols]
 df.to_csv('지역화폐_결과.csv', index=False, encoding = 'utf-8-sig')
 
 print('완료!')
